cc35243e88cbc633772cd200fa490f65dd8812a8
nheingit <60185486+nheingit@users.noreply.github.com>
2024-07-19 13:29:31 -0500

    format doc

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -8,24 +8,25 @@ load_dotenv()  # take environment variables from .env.
 
 tg_bot_token = os.getenv("TG_BOT_TOKEN")
 
-
-messages = [{
-  "role": "system",
-  "content": "You are a helpful assistant that answers questions."
-}]
+messages = [
+    {"role": "system", "content": "You are a helpful assistant that answers questions."}
+]
 
 logging.basicConfig(
-  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-  level=logging.INFO)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="I'm a bot, please talk to me!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
 
-if __name__ == '__main__':
-  application = ApplicationBuilder().token(tg_bot_token).build()
 
-  start_handler = CommandHandler('start', start)
-  application.add_handler(start_handler)
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(tg_bot_token).build()
 
-  application.run_polling()
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
