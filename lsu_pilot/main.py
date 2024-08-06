334611788676898684d1559399c821f9b74c7876
nheingit <60185486+nheingit@users.noreply.github.com>
2024-08-05 21:39:39 -0500

    embeddings

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -4,11 +4,21 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 from openai import OpenAI
+import pandas as pd
+import numpy as np
+from .questions import answer_question
 
 load_dotenv()  # take environment variables from .env.
 
 tg_bot_token = os.getenv("TG_BOT_TOKEN")
 openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the absolute path to the CSV file
+csv_path = os.path.join(current_dir, "processed", "embeddings.csv")
+df = pd.read_csv(csv_path, index_col=0)
+df["embeddings"] = df["embeddings"].apply(eval).apply(np.array)
 
 
 messages = [
@@ -26,6 +36,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    answer = answer_question(df, question=update.message.text, debug=True)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+
+
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     messages.append({"role": "user", "content": update.message.text})
     completion = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
@@ -41,8 +56,10 @@ if __name__ == "__main__":
 
     start_handler = CommandHandler("start", start)
     chat_handler = CommandHandler("chat", chat)
+    mozilla_handler = CommandHandler("mozilla", mozilla)
 
     application.add_handler(start_handler)
     application.add_handler(chat_handler)
+    application.add_handler(mozilla_handler)
 
     application.run_polling()
