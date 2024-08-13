6bf5493a0e0e5244bcd14d230641961593f6d16e
nheingit <60185486+nheingit@users.noreply.github.com>
2024-08-13 09:42:00 -0500

    image generation

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -13,6 +13,7 @@ from openai import OpenAI
 import pandas as pd
 import numpy as np
 import json
+import requests
 
 from .questions import answer_question
 from .functions import functions, run_function
@@ -86,6 +87,16 @@ async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
     answer = answer_question(df, question=update.message.text, debug=True)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
+async def image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  response = openai.images.generate(prompt=update.message.text,
+                                    model="dall-e-3",
+                                    n=1,
+                                    size="1024x1024")
+  image_url = response.data[0].url
+  image_response = requests.get(image_url)
+  await context.bot.send_photo(chat_id=update.effective_chat.id,
+                               photo=image_response.content)
+
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     messages.append({"role": "user", "content": update.message.text})
@@ -154,9 +165,11 @@ if __name__ == "__main__":
     start_handler = CommandHandler("start", start)
     chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
     mozilla_handler = CommandHandler("mozilla", mozilla)
+    image_handler = CommandHandler('image', image)
 
     application.add_handler(start_handler)
     application.add_handler(chat_handler)
+    application.add_handler(image_handler)
     application.add_handler(mozilla_handler)
 
     application.run_polling()
